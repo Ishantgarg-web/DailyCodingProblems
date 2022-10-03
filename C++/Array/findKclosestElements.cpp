5fc6c8c959f95bdfe9c664a840572a9a5f5a43c5
priddhesh <109747774+priddhesh@users.noreply.github.com>
2022-10-03 23:09:53 +0530

    Create findKclosestElements.cpp

---
+++ C++/Array/findKclosestElements.cpp
@@ -0,0 +1,15 @@
+vector<int> findClosestElements(vector<int>& A, int k, int x) {
+        int l = 0, r = A.size() - k;
+        while (l < r) {
+            int middle = (left + right) / 2;
+            if (x - A[middle] > A[middle + k] - x)
+            {
+                l = middle + 1;
+            }
+            else
+            {
+                r = middle;
+            }
+        }
+        return vector<int>(A.begin() + l, A.begin() + l + k);
+    }
