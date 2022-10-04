43a7c918755d871bd313d435fb046dbc3899086b
Faizan96322 <72128134+Faizan96322@users.noreply.github.com>
2022-10-04 20:52:43 +0530

    PascalsTriangle 1 & 2

---
+++ Python/Array/PascalsTraingle(118).py
@@ -0,0 +1,24 @@
+#https://leetcode.com/problems/pascals-triangle/
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result=[]
+        for i in range(numRows):
+            if(i ==0):
+                row = [1]
+                result.append(row)
+            else:
+                row1 = [1]
+                j = 1
+                while(j < i):
+                    row1.append(row[j-1] + row[j])
+                    j+=1
+                row1.append(1)
+                result.append(row1)
+                row = row1
+        return result
+
